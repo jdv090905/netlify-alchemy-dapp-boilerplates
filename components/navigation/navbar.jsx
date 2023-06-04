bc25c816aebf53230cab8e1f024fcdcf16332303
jdv090905 <86566557+jdv090905@users.noreply.github.com>
2023-06-04 12:20:03 +0800

    Add files via upload

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,7 +3,7 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
+			<a href="https://www.avito.ru/user/505ee82d4dfaa0bdcfbfe4e2fc5bb305/profile?id=2956908562&iid=2956908562&src=messenger&page_from=from_item_messenger" target={"_blank"}>
 				<img className={styles.alchemy_logo} src="/cw3d-logo.png"></img>
 			</a>
 			<ConnectButton></ConnectButton>
